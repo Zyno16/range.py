b0b6a924963802ed38d6c9015bd7303bfb9a61e5
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-10 18:40:11 +0000

    Add files via upload

---
+++ range.py
@@ -0,0 +1,10 @@
+friends =["zino","yusra","ines"]
+print(len(friends))
+print(range(len(friends)))
+
+for i in range(len(friends)):
+    friend =friends[i]
+    print("ramadan mubarek" ,friend)
+    
+
+
